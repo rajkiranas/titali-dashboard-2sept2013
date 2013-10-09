7cf4a9c1d021c8d75ff629a83f6b9b5a1279d8fc
rajkiranas@gmail.com <rajkiranas@gmail.com@423b66fb-f55a-3616-cadb-11499509e9dd>
2013-10-09 13:08:09 +0000

    ui change for learn screen listing

--- src/main/java/com/quick/table/StudQuickLearnTable.java
+++ src/main/java/com/quick/table/StudQuickLearnTable.java
@@ -4,16 +4,22 @@
  */
 package com.quick.table;
 
+import com.quick.bean.ForumEventDetailsBean;
 import com.quick.bean.MasteParmBean;
 import com.quick.container.StudQuickLearnContainer;
 import com.quick.data.MyDashBoardContainer;
+import com.quick.forum.ForumDetailWraper;
 import com.quick.global.GlobalConstants;
+import com.quick.ui.QuickLearn.QuickLearnDetailWraper;
 import com.vaadin.data.Property;
 import com.vaadin.demo.dashboard.StudQuickLearn;
+import com.vaadin.event.ItemClickEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.BaseTheme;
+import java.util.List;
 
 /**
  *
@@ -27,17 +33,30 @@ public class StudQuickLearnTable extends Table {
         //setSizeFull();
       //  addStyleName("plain");
         addStyleName("borderless");
+        addStyleName("plain");
         setSortEnabled(false);
         setWidth("100%");
+        setHeight("500px");
         setPageLength(10);
         setSelectable(true);
-        setImmediate(true); // react at once when something is selected
-        setContainerDataSource(StudQuickLearnContainer.getStudQuickLearnContainer(quickLearn.getTopicList()));
-        setVisibleColumns(StudQuickLearnContainer.NATURAL_COL_ORDER_QUICKLEARN);
-        setColumnHeaders(StudQuickLearnContainer.COL_HEADERS_ENGLISH_QUICKLEARN);
-        addValueChangeListener((Property.ValueChangeListener)quickLearn);
+        setImmediate(true);// react at once when something is selected
+        setSortEnabled(false);
+        addContainerProperty(GlobalConstants.emptyString, VerticalLayout.class, null);
         
-        addGeneratedColumn("View details", new Table.ColumnGenerator() 
+        List<MasteParmBean> masterParamList = quickLearn.getTopicList();
+        
+        for(MasteParmBean topicDetails:masterParamList)
+        {
+            addItem(new Object[]{new QuickLearnDetailWraper(topicDetails,quickLearn) },size()+1);
+        }
+//        setContainerDataSource(StudQuickLearnContainer.getStudQuickLearnContainer(quickLearn.getTopicList()));
+//        setVisibleColumns(StudQuickLearnContainer.NATURAL_COL_ORDER_QUICKLEARN);
+//        setColumnHeaders(StudQuickLearnContainer.COL_HEADERS_ENGLISH_QUICKLEARN);
+        
+        //addValueChangeListener((Property.ValueChangeListener)quickLearn);
+        
+        
+        /* addGeneratedColumn("View details", new Table.ColumnGenerator() 
             {
             @Override
                 public Object generateCell(Table source, Object rowItemBean, Object columnId) 
@@ -49,7 +68,7 @@ public class StudQuickLearnTable extends Table {
                     quickLearn.addListenertoBtn(btnRemove);               
                     return btnRemove;
                 }
-            });
+            }); */
        
         }
     
