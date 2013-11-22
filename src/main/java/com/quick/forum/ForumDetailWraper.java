6c8fd2304b2b098e72da9557c7fac198a314131e
rajkiranas@gmail.com <rajkiranas@gmail.com@423b66fb-f55a-3616-cadb-11499509e9dd>
2013-11-22 12:34:27 +0000

    wrapper heading changed

--- src/main/java/com/quick/forum/ForumDetailWraper.java
+++ src/main/java/com/quick/forum/ForumDetailWraper.java
@@ -4,29 +4,20 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.quick.bean.*;
-import com.vaadin.demo.dashboard.*;
-import java.text.SimpleDateFormat;
-
-import com.quick.data.DataProvider.Movie;
-import com.quick.entity.ForumEventDetails;
 import com.quick.global.GlobalConstants;
 import com.quick.utilities.DateUtil;
 import com.quick.utilities.MyImageSource;
-import com.quick.utilities.UIUtils;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.Base64;
 import com.vaadin.event.MouseEvents;
 import com.vaadin.event.ShortcutAction;
-import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.event.ShortcutListener;
 import com.vaadin.event.dd.DragAndDropEvent;
 import com.vaadin.event.dd.DropHandler;
 import com.vaadin.event.dd.acceptcriteria.AcceptAll;
 import com.vaadin.event.dd.acceptcriteria.AcceptCriterion;
-import com.vaadin.server.ExternalResource;
-import com.vaadin.server.Resource;
 import com.vaadin.server.StreamResource;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.shared.ui.label.ContentMode;
@@ -35,13 +26,9 @@ import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.DragAndDropWrapper.DragStartMode;
 import com.vaadin.ui.themes.BaseTheme;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
@@ -177,11 +164,11 @@ public class ForumDetailWraper extends VerticalLayout {
 //        }
 
 
-        String cap = "<b>" + eventDetails.getEventDesc() + "</b>" + " by <b>"
-                + eventDetails.getEventOwner() + "</b>";
+        String cap = "<div style='color:deeppink;display:inline-block;'> <b>" + eventDetails.getEventOwner() + "</b></div>" + "<div style='color:grey;font-size:13px;display:inline-block;'>&nbsp; shared </div> <div style='color:deeppink;display:inline-block;'>"
+                + eventDetails.getEventDesc() + "</div>";
         label = new Label(cap, ContentMode.HTML);
         label.setWidth("100%");
-        label.setStyleName("deepPinkColor");
+        //label.setStyleName("deepPinkColor");
         //label.setCaption("");
         fields.addComponent(label);
 
