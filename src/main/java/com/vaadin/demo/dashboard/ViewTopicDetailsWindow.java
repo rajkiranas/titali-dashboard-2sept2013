009eb1871c0a2ea077aa235b58cbd10a1033a55a
rajkiranas@gmail.com <rajkiranas@gmail.com@423b66fb-f55a-3616-cadb-11499509e9dd>
2014-06-04 18:06:02 +0000

    changes for adding video in dashboard activity
    1) topic handling done - half
    2) forum handling remaining

--- src/main/java/com/vaadin/demo/dashboard/ViewTopicDetailsWindow.java
+++ src/main/java/com/vaadin/demo/dashboard/ViewTopicDetailsWindow.java
@@ -11,6 +11,7 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.quick.utilities.UIUtils;
+import com.quick.utilities.VideoUtil;
 import com.vaadin.event.FieldEvents;
 import com.vaadin.event.FieldEvents.BlurEvent;
 import com.vaadin.shared.ui.MarginInfo;
@@ -179,64 +180,7 @@ public class ViewTopicDetailsWindow extends Window implements Button.ClickListen
 
             String videoId = quickLearnPojo.getVideoPath();
 
-            if (videoId.indexOf("=") > 0) {
-                videoId = videoId.substring(videoId.lastIndexOf("=") + 1);
-            } else {
-                videoId = videoId.substring(videoId.lastIndexOf("/") + 1);
-            }
-            //System.out.println("********videoId="+videoId);
-            String str = "<!DOCTYPE html>\n"
-                    + "<html>\n"
-                    + "  <body>\n"
-                    + "    <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->\n"
-                    + "<iframe id=\"player\" type=\"text/html\" width=\"400\" height=\"250\"\n"
-                    + "  src=\"https://www.youtube.com/embed/" + videoId + "?enablejsapi=1&origin=https://example.com\"\n"
-                    + "  frameborder=\"0\"></iframe>"
-                    + "\n"
-                    + "    <script>\n"
-                    + "      // 2. This code loads the IFrame Player API code asynchronously.\n"
-                    + "      var tag = document.createElement('script');\n"
-                    + "\n"
-                    + "      tag.src = \"https://www.youtube.com/iframe_api\";\n"
-                    + "      var firstScriptTag = document.getElementsByTagName('script')[0];\n"
-                    + "      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);\n"
-                    + "\n"
-                    + "      // 3. This function creates an <iframe> (and YouTube player)\n"
-                    + "      //    after the API code downloads.\n"
-                    + "      var player;\n"
-                    + "      function onYouTubeIframeAPIReady() {\n"
-                    + "        player = new YT.Player('player', {\n"
-                    + "          height: '250',\n"
-                    + "          width: '400',\n"
-                    + "          videoId: '" + videoId + "',\n"
-                    + "          events: {\n"
-                    + "            'onReady': onPlayerReady,\n"
-                    + "            'onStateChange': onPlayerStateChange\n"
-                    + "          }\n"
-                    + "        });\n"
-                    + "      }\n"
-                    + "\n"
-                    + "      // 4. The API will call this function when the video player is ready.\n"
-                    + "      function onPlayerReady(event) {\n"
-                    + "        event.target.playVideo();\n"
-                    + "      }\n"
-                    + "\n"
-                    + "      // 5. The API calls this function when the player's state changes.\n"
-                    + "      //    The function indicates that when playing a video (state=1),\n"
-                    + "      //    the player should play for six seconds and then stop.\n"
-                    + "      var done = false;\n"
-                    + "      function onPlayerStateChange(event) {\n"
-                    + "        if (event.data == YT.PlayerState.PLAYING && !done) {\n"
-                    + "          setTimeout(stopVideo, 6000);\n"
-                    + "          done = true;\n"
-                    + "        }\n"
-                    + "      }\n"
-                    + "      function stopVideo() {\n"
-                    + "        player.stopVideo();\n"
-                    + "      }\n"
-                    + "    </script>\n"
-                    + "  </body>\n"
-                    + "</html>";
+            String str = VideoUtil.getYoutubeEmbedingString(videoId);
 
             Label video = new Label(str, ContentMode.HTML);
 
